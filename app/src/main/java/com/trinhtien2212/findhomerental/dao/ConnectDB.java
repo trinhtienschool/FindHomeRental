4e2bee7c140899f777452fe1b577d13533ddd247
trinhtienschool <18130243@st.hcmuaf.edu.vn>
2021-06-19 09:32:35 +0700

    delete

--- app/src/main/java/com/trinhtien2212/findhomerental/dao/ConnectDB.java
+++
@@ -1,7 +0,0 @@
-package com.trinhtien2212.findhomerental.dao;
-
-import com.google.firebase.firestore.FirebaseFirestore;
-
-public abstract class ConnectDB {
-    protected FirebaseFirestore db = FirebaseFirestore.getInstance();
-}
--- app/src/main/java/com/trinhtien2212/findhomerental/presenter/RoomsResult.java
+++
@@ -1,9 +0,0 @@
-package com.trinhtien2212.findhomerental.presenter;
-
-import com.trinhtien2212.findhomerental.model.Room;
-
-import java.util.List;
-
-public interface RoomsResult {
-     void returnRooms(List<Room> rooms);
-}
