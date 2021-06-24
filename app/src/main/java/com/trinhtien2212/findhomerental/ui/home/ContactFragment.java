bc6e910c12ea98c9b2e7c7ad7fb8f4fbe813590b
trinhtienschool <18130243@st.hcmuaf.edu.vn>
2021-06-24 21:35:59 +0700

    Add Contact fragment

---
+++ app/src/main/java/com/trinhtien2212/findhomerental/ui/home/ContactFragment.java
@@ -0,0 +1,35 @@
+package com.trinhtien2212.findhomerental.ui.home;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.trinhtien2212.findhomerental.MainActivity;
+import com.trinhtien2212.findhomerental.R;
+
+
+public class ContactFragment extends Fragment {
+
+    private View view;
+    private MainActivity mainActivity;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view =  inflater.inflate(R.layout.fragment_contact, container, false);
+        //Todo Hang
+        textZalo=view.findViewById(R.id.text_zalo);
+        mainActivity = (MainActivity) getActivity();
+
+
+
+        return view;
+    }
+
+
+
+}
